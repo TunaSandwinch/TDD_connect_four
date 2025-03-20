ace4b778c8cc4447d86f586ce89d9393aac4b4c8
TunaSandwinch <winch.mascarenas@gmail.com>
2025-03-20 20:59:23 +0800

    feat: add player_input and valid_input? methods that accepts and verify a player input

---
+++ lib/game.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'player'
+require_relative 'board'
+# A class for the whole connect four game that use the player and board object
+class ConnectFour
+  def initialize(player1 = Player.new, player2 = Player.new, board = Board.new)
+    @player1 = player1
+    @player2 = player2
+    @board = board
+  end
+
+  def valid_input?(input)
+    input.to_i.between?(1, 7)
+  end
+
+  def player_input
+    loop do
+      puts 'enter a number from 1-7'
+      input = gets.chomp
+      return input if valid_input?(input)
+
+      puts 'Invalid Input!'
+    end
+  end
+end
