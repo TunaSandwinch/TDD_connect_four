500d0fe20178939f9495ade72a830c70ad3021f2
TunaSandwinch <winch.mascarenas@gmail.com>
2025-03-22 16:46:46 +0800

    feat: add tests for y_coordinate method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,12 +12,12 @@ describe ConnectFour do # rubocop:disable Metrics/BlockLength
       expect(player1).to be_a(Player)
     end
 
-    it 'has player2 instance variable with a player object value' do
+    it 'has player2 instance variable with a Player object value' do
       player2 = game.instance_variable_get(:@player2)
       expect(player2).to be_a(Player)
     end
 
-    it 'has a board instance variable with a board object value' do
+    it 'has a board instance variable with a Board object value' do
       board = game.instance_variable_get(:@board)
       expect(board).to be_a(Board)
     end
@@ -78,7 +78,21 @@ describe ConnectFour do # rubocop:disable Metrics/BlockLength
     end
   end
 
-  describe '#player_input' do
+  describe '#player_input' do # rubocop:disable Metrics/BlockLength
+    it 'returns integer data type' do
+      allow(game).to receive(:gets).and_return('5')
+      allow(game).to receive(:puts).and_return('enter a number from 1 - 7')
+      result = game.player_input
+      expect(result).to be_an(Integer)
+    end
+
+    it 'returns an integer value that is minus 1 from the original input' do
+      allow(game).to receive(:gets).and_return('1')
+      allow(game).to receive(:puts).and_return('enter a number from 1 - 7')
+      result = game.player_input
+      expect(result).to eq(0)
+    end
+
     context 'when user typed two invalid inputs' do
       it 'display error message two times' do
         allow(game).to receive(:gets).and_return('winch', '100', '5')
@@ -94,6 +108,61 @@ describe ConnectFour do # rubocop:disable Metrics/BlockLength
         expect(game).to receive(:puts).with('enter a number from 1-7').once
         expect(game).not_to receive(:puts).with('Invalid Input!')
         game.player_input
+      end
+    end
+  end
+
+  describe 'y_coordinate' do # rubocop:disable Metrics/BlockLength
+    context 'when board is empty' do
+      let(:player1) { double('player') }
+      let(:player2) { double('player') }
+      grid_val = Array.new(6) { Array.new(7) { '' } }
+      let(:board) { double('board', grid: grid_val) }
+      subject(:game_coordinate) { described_class.new(player1, player2, board) }
+
+      it 'returns 5 if x_coordinate = 0' do
+        result = game_coordinate.y_coordinate(0)
+        expect(result).to eq(5)
+      end
+
+      it 'returns 5 if x_coordinate = 3' do
+        result = game_coordinate.y_coordinate(3)
+        expect(result).to eq(5)
+      end
+
+      it 'return 5 if x_coordinate = 6' do
+        result = game_coordinate.y_coordinate(6)
+        expect(result).to eq(5)
+      end
+    end
+
+    describe 'when row 3-5 is full' do
+      let(:player1) { double('player') }
+      let(:player2) { double('player') }
+      grid_val = [
+        Array.new(7) { '' },
+        Array.new(7) { '' },
+        Array.new(7) { '' },
+        Array.new(7) { '#' },
+        Array.new(7) { '#' },
+        Array.new(7) { '#' }
+      ]
+      let(:board) { double('board', grid: grid_val) }
+      subject(:game_coordinate) { described_class.new(player1, player2, board) }
+
+      it 'returns 2 if x_coordinate = 0' do
+        result = game_coordinate.y_coordinate(0)
+        expect(result).to eq(2)
+      end
+
+      it 'returns 2 if x_coordinate = 3' do
+        result = game_coordinate.y_coordinate(3)
+        expect(result).to eq(2)
+      end
+
+      it 'returns 2 if x_coordinate = 6' do
+        result = game_coordinate.y_coordinate(6)
+        expect(result).to eq(2)
       end
     end
   end
